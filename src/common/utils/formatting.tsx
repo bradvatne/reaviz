39ee725b6488a09c430598a817134873abcffa9a
Austin McDaniel <amcdaniel2@gmail.com>
2019-03-01 13:36:00 -0600

    remove moment

--- src/common/utils/formatting.tsx
+++ src/common/utils/formatting.tsx
@@ -1,15 +1,12 @@
 import { ChartInternalDataTypes } from '../data';
 import { isNumber, isDate } from 'lodash-es';
-import moment from 'moment';
 
 /**
  * Format a value based on type.
  */
 export function formatValue(value: ChartInternalDataTypes): string {
   if (value !== undefined) {
-    if (isDate(value)) {
-      return moment(value).format('MM/DD/YY');
-    } else if (isNumber(value)) {
+    if (isDate(value) || isNumber(value)) {
       return value.toLocaleString();
     }
 
